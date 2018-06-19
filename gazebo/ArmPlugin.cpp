3883d8f83d69b2bb947a03fcbdeb6bd49e2ea76e
luz211 <zlt1213@gmail.com>
2018-06-19 11:15:43 +0800

    hyperparameters tuned

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -35,14 +35,14 @@
 /
 */
 
-#define INPUT_WIDTH   512
-#define INPUT_HEIGHT  512
-#define OPTIMIZER "None"
-#define LEARNING_RATE 0.0f
+#define INPUT_WIDTH   64
+#define INPUT_HEIGHT  64
+#define OPTIMIZER "RMSprop"
+#define LEARNING_RATE 0.01f
 #define REPLAY_MEMORY 10000
-#define BATCH_SIZE 8
-#define USE_LSTM false
-#define LSTM_SIZE 32
+#define BATCH_SIZE 128
+#define USE_LSTM true
+#define LSTM_SIZE 16
 
 /*
 / TODO - Define Reward Parameters
