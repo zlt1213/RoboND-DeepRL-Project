bbb1f5626da5c1ee43f15e18f3dd6fddc41d8aad
luz211 <zlt1213@gmail.com>
2018-06-19 14:04:18 +0800

    d

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -38,11 +38,11 @@
 #define INPUT_WIDTH   64
 #define INPUT_HEIGHT  64
 #define OPTIMIZER "Adam"
-#define LEARNING_RATE 0.1f
+#define LEARNING_RATE 0.08f
 #define REPLAY_MEMORY 10000
 #define BATCH_SIZE 256
 #define USE_LSTM true
-#define LSTM_SIZE 256
+#define LSTM_SIZE 128
 
 /*
 / TODO - Define Reward Parameters
